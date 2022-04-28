273be6cbddae78247a6d39c2cee2c2ce289ed4a8
AnneC.Yates <annecreeyates@gmail.com>
2022-04-28 22:46:31 +0100

    Changed index.php classes to enable modular approach in CSS; removes all previous CSS elements -- this is the beginning of a completly new approach

--- index.php
+++ index.php
@@ -4,17 +4,19 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="styles.css" type="text/css" media="screen">
-
+  
   <!-- Font Awsome icons -->
+  <link rel="stylesheet" href="styles.css" type="text/css" media="screen">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
   <!-- fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Poiret+One&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Allison&family=Palette+Mosaic&display=swap" rel="stylesheet">
 
+
+  <!-- make the title more gripping, more key words etc. for the purpose of SEO -->
   <title>
-    Anne C. Yates
+    Anne C. Yates | London | Front End Developer Portfolio
   </title>
 </head>
 
@@ -24,111 +26,104 @@
   include("form-to-email.php");
   ?>
 
-  <header class="header">
-    <nav id="navbar" class="navbar">
-      <!-- <a href="#" class="nav-logo">Anne Cree Yates</a> -->
-      <ul class="nav-menu">
-        <!-- <li class="nav-item">
-                    <a href="#home" class="nav-link">Home</a>
-                </li> -->
-        <li class="nav-item">
-          <a href="#about" class="nav-link">About</a>
+    <nav class="main-nav">
+
+      <ul class="main-nav__menu">
+        <li class="main-nav__item">
+          <a href="#about" class="main-nav__about-link">About</a>
         </li>
-        <li class="nav-item">
-          <a href="#work" class="nav-link">Work</a>
+        <li class="main-nav__item">
+          <a href="#work" class="main-nav__work-link">Work</a>
         </li>
-        <li class="nav-item">
-          <a href="#contact" class="nav-link">Contact</a>
+        <li class="main-nav__item">
+          <a href="#contact" class="main-nav__contact-link">Contact</a>
         </li>
       </ul>
-      <div class="hamburger">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-      </div>
-    </nav>
-  </header>
 
-  <section class="introduction" id="introduction">
-    <div class="framed-image">
-      <h1>Hello!</h1>
-      <img class="image" src="./imgs/polaroid-and-flowers.png">
-      <!--image of the frame around the photo-->
-      <img class="image2" src="./imgs/pontu.gif">
-      <h3> I'm Anna</h3>
-    </div>
-    <h2>Front End Developer</h2><br>
-    <h4>located in <span>London</span></h4>
+      <div class="hamburger-bars">
+        <span class="top-bar"></span>
+        <span class="middle-bar"></span>
+        <span class="bottom-bar"></span>
+      </div>
+
+    </nav>
+
+
+  <section class="intro" id="intro">
+
+      <header>
+        <p class = "intro__hello">Hello!</p>
+        <p class = "intro__name"> I'm Anna</p>
+        <p class = "intro__title">Front End Developer</p>
+        <p class = "intro__location">located in <span>London</span></p>
+      </header>
+
+    <figure class="intro__images">
+      <img class="image-frame" src="./imgs/polaroid-and-flowers.png">
+      <img class="image-hero" src="./imgs/pontu.gif">
+    </figure>
+    
   </section>
 
-  <article class="about" id="about">
-    <h3>About me</h3>
-    <div class="container">
+  <section class="about" id="about">
+    <div class="about__wrapper">
+      <h1>About me</h1>
       <p>
         Like in every case there is a short and a long version of my story.
         The short goes like this:
       </p>
-      <br>
 
       <ul>
         <li>I am an artist that has found in coding a new form of expression.</li>
         <li>Trouble shooting is my second nature, so I try to solve any issue I can put my hands (and my brain-cycles) on.</li>
         <li>I work with a range of technologies, including (but not exclusively) <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong> as well as <strong>PHP</strong>, and I constantly develop my already existing ones and new skills.</li>
       </ul>
-      <br>
+      
       <p>
         If you wish to find out more here is the longer version.
-        If you have any questions go ahead and <br><a href=#contact class="contact-link"> get in touch.</a>
+        If you have any questions go ahead and <a href=#contact class="contact-link"> get in touch.</a>
       </p>
 
 
     </div>
-  </article>
+</section>
 
   <section class="work" id="work">
-    <div class="description">
-      <h3>Latest Project</h3>
-      <p><span>Project title goes here - this is a placeholder</span><br>
-        A lot of Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Itaque eos, explicabo odio quis ipsa, neque modi et voluptate voluptatibus assumenda
-        molestiae similique, doloremque veritatis laborum omnis dignissimos reiciendis
-        atque maxime!
-        <br>
-        <br>
-        <a href="https://annecreeyates.com/" target="_blank">
-          <img class=project-image src="imgs/pontu.gif" alt="Anne Cree Yates Shop website" />
-          <!--the jpg with the first glance of the website goes here-->
-        </a>
-      </p>
+    <div class="work__wrapper">
+      <h1>Latest Project</h1>
+      <h2 class = "work__project-title">Project title goes here - this is a placeholder</h2>
+        <p class = "work__project-description">
+          A lot of Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Itaque eos, explicabo odio quis ipsa, neque modi et voluptate voluptatibus assumenda
+          molestiae similique, doloremque veritatis laborum omnis dignissimos reiciendis
+          atque maxime!
+          <a href=" " target="_blank">
+            <img class = work__project-image src="imgs/pontu.gif" alt=""/>
+            <!--the jpg with the first glance of the website goes here-->
+          </a>
+        </p>
       <!--This is a placeholder for a description of the latest project. 
                 Let people know what exactly is it, like "this is a website of a so-and-so artist/ business",
             the reason for building it "bringing the wonderful aretwork to the world, etc", what techology was used to build it, like html, css, js, react etc,
         and make sure the website is live before making it go live. And finally add the link under the description. Should show up as a thumbnail.-->
-
-
-
     </div>
   </section>
 
   <section class="contact" id="contact">
-    <div>
-      <h3>Let's get in touch!</h3>
+    <div class = "contact__wrapper">
+      <h1>Let's get in touch!</h1>
 
-      <div class="contact-form">
-        <form id="contact-form" method="post" action="/form-to-email.php" name="formToEmail">
+      <div class="contact__form-wrapper">
+        <form class="contact__form" method="post" action="/form-to-email.php" name="formToEmail">
 
           <?php echo ((!empty($errorMessage)) ? $errorMessage : '') ?>
 
-          <input name="name" type="text" class="form-controller" placeholder="Your name" required>
-          <br>
-          <input name="email" type="email" class="form-controller" placeholder="Your email" required>
-          <br>
-          <input name="subject" type="text" class="form-controller" id="subject" placeholder="Subject" required>
-          <br>
-          <textarea name="message" class="form-controller, message" placeholder="Your message" required></textarea>
-          <br>
+          <input name="name" type="text" class="contact__details, name" placeholder="Your name" required>
+          <input name="email" type="email" class="contact__details, email" placeholder="Your email" required>
+          <input name="subject" type="text" class="contact__details, subject" placeholder="Subject" required>
+          <textarea name="message" class="contact__details, message" placeholder="Your message" required></textarea>
 
-          <input type="submit" class="form-controller-submit" value="Send">
+          <input type="submit" class="contact__details, submit" value="Send">
 
         </form>
 
@@ -145,11 +140,10 @@
     <li><a href="https://twitter.com/CreeYates" target=_blank class="fa fa-twitter"></a></li>
     <li><a href="https://www.linkedin.com/in/annawrzosek/" target=_blank class="fa fa-linkedin"></a></li>
   </ul>
-  <!--think if you want to add some other social media later-->
+  <!--think if you want to add some other social media later - github-->
 
   <p class="references">
     Resources:
-    <br>
     <a href='https://pngtree.com/so/polaroid'>polaroid png from pngtree.com/</a>
   </p>
 
