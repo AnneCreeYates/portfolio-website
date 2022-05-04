e4a8b1ff1cc7632b75924c0df5c240e5f5b50dc0
Anne C. Yates <annecreeyates@gmail.com>
2022-05-04 16:01:13 +0100

    corrected a spelling mistake in gitignore file; in gulp file changed the watcher from just index.scss to sass file, also added errror message; in index.php: changed font families; moved index.scss to the scss folder; introduced new files for main nav, base, font and colors - imported and tested -works fine

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const {src, dest, watch, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles(){
-  return src('index.scss')
-  .pipe(sass())
+  return src('sass_files/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
   .pipe(dest('css'))
 }
 
 function watchTask() {
-    watch(['index.scss'], buildStyles)
+    watch(['sass_files/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
--- index.php
+++ index.php
@@ -10,8 +10,9 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
   <!-- fonts -->
-  <link href="https://fonts.googleapis.com/css2?family=Poiret+One&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css2?family=Allison&family=Palette+Mosaic&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;300;400;500&family=Water+Brush&display=swap" rel="stylesheet"> 
 
 
   <!-- make the title more gripping, more key words etc. for the purpose of SEO -->
