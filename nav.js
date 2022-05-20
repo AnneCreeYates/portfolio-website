15648cbb1e7682aea2b6096d8b7e359ec0428ee9
Anne C. Yates <annecreeyates@gmail.com>
2022-05-20 21:12:51 +0100

    I'm experimenting with background images --> now it not optimised one, to check the page loading speed; in introduction added the modules and stated working on media queries; in main nav: changed the width to fit-content and switched the visibility to none, height: 0 when closed -- when menu is open changed to display block, visibility visible and transition ease-in-out; in modules added the media query mixin

--- nav.js
+++ nav.js
@@ -2,8 +2,9 @@ const hamburger = document.querySelector(".hamburger-bars")
 const mainNavToggle = document.querySelector(".main-nav")
 const mainNavItem = document.querySelectorAll(".main-nav__item")
 
-mainNavToggle.addEventListener("click", e => {
+mainNavToggle.addEventListener("click", _ => {
     document.body.classList.toggle("is-open")
+
 })
 
 
