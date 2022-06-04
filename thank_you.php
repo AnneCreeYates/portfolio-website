c03774ab09531e86384d7a2002be1225d5eea346
Anne C. Yates <annecreeyates@gmail.com>
2022-06-04 19:36:41 +0100

    Added the head and content (text and button) to thank-you.php; in thank-you.php: added the background, styled text and the button; connected all the files --> everything seems to be working

--- thank_you.php
+++ thank_you.php
@@ -1,3 +1,41 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="/css/index.css" type="text/css" media="screen">
+  <!-- Font Awsome icons -->
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed&display=swap" rel="stylesheet">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Zen+Loop&display=swap" rel="stylesheet">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600&display=swap" rel="stylesheet"> 
+
+
+
+  <title>
+    Anne C. Yates | London | Front End Developer Portfolio
+  </title>
+</head>
+
+<body class = "thank-you">
 <?php ?>
-<h1>Thank you for sending the message!</h1>
-<?php ?>
+<div class = "thank-you-message">
+    <h1>Thank you for sending the message!</h1>
+    <p>I will respond as soon as I can.</p>
+
+    <div class = "return-button__bubble">
+        <a class = "return-button" href="index.php">Return to the Website</a>
+    </div>
+</div>
+</body>
+</html>
